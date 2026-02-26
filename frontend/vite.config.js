49aeabfae4a0de1986d7c004718498801cfeeada
quynhnhu3704 <22663381.nhu@student.iuh.edu.vn>
2026-02-26 10:47:00 +0700

    chore: push full project structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // cho phép truy cập từ ngoài container
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://backend:5000", // dùng tên service trong docker-compose
         changeOrigin: true
       }
     }
